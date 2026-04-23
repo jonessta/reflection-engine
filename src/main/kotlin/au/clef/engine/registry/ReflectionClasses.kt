3498699f1bcdb226944848865dd4aabd41f3be8b
David Jones <34013850+jonessta@users.noreply.github.com>
2026-04-23 12:13:36 +1000

    new

--- src/main/kotlin/au/clef/engine/registry/ReflectionClasses.kt
+++ src/main/kotlin/au/clef/engine/registry/ReflectionClasses.kt
@@ -1,20 +1,11 @@
 package au.clef.engine.registry
 
 // todo does this extend RegisteredClasses
-interface ReflectionClasses {
+interface ReflectionClasses: RegisteredClasses {
 
     /**
      * Classes of static methods or class od an instance that if the facade that will be invoked. Eg Math.min(int,int)
      * or instance of a service val myService = MyService() - with class MyService::class
      */
     val targetClasses: List<Class<*>>
-
-    /**
-     * All classes supporting reflection, including target classes. Supporting classes are method composite structures
-     * like Person class in the following:
-     * myService = MyService()
-     * val person1 = new Person("David", 23, MALE)
-     * myService.addPerson(person1)
-     */
-    val classes: List<Class<*>>
 }

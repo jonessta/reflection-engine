cab51a06ebafc801128b082ce4a9c347f0c76897
David Jones <34013850+jonessta@users.noreply.github.com>
2026-04-24 10:00:56 +1000

    new

--- src/main/kotlin/au/clef/engine/ExposedTarget.kt
+++ src/main/kotlin/au/clef/engine/ExposedTarget.kt
@@ -5,18 +5,26 @@ import kotlin.reflect.KClass
 
 sealed class ExposedTarget {
 
+    abstract val targetClass: KClass<*>
+
     /**
      * Expose all supported static methods on this class.
      */
-    data class StaticClass(val clazz: KClass<*>) : ExposedTarget()
+    data class StaticClass(override val targetClass: KClass<*>) : ExposedTarget()
 
     /**
      * Expose exactly one static method.
      */
-    data class StaticMethod(val methodId: MethodId) : ExposedTarget()
+    data class StaticMethod(private val methodId: MethodId) : ExposedTarget() {
+        override val targetClass: KClass<*>
+            get() = methodId.declaringClass.kotlin
+    }
 
     /**
      * Expose instance methods on this object via its id.
      */
-    data class Instance(val id: String, val obj: Any) : ExposedTarget()
+    data class Instance(val id: String, val obj: Any) : ExposedTarget() {
+        override val targetClass: KClass<*>
+            get() = obj::class
+    }
 }

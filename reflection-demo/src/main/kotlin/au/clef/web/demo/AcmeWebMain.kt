1b8f66b969f067e73d1d194b6c1b075562afcccf
David Jones <34013850+jonessta@users.noreply.github.com>
2026-04-26 08:35:03 +1000

    new

--- reflection-demo/src/main/kotlin/au/clef/web/demo/AcmeWebMain.kt
+++ reflection-demo/src/main/kotlin/au/clef/web/demo/AcmeWebMain.kt
@@ -7,15 +7,21 @@ import au.clef.web.WebServerConfig
 
 
 import au.clef.app.demo.model.AcmeService
+import au.clef.app.demo.model.Person
 import au.clef.app.demo.model.add
 import au.clef.engine.ExposedTarget
+import au.clef.engine.model.MethodId
 
 fun main() {
+    val acmeService = AcmeService()
     val reflectionService = ReflectionServiceApi(
         targets = listOf(
-            ExposedTarget.Instance(obj = AcmeService(), id = "acmeService"),
+//            ExposedTarget.Instance(acmeService),
+            ExposedTarget.InstanceMethod(acmeService, MethodId.from(AcmeService::class, "personName", Person::class)),
+
             ExposedTarget.StaticMethod.from(::add),
             ExposedTarget.StaticMethod.from(Math::class, "max", Int::class, Int::class)
+//            ExposedTarget.StaticClass(Math::class)
         ),
         targetSupportingTypes = AcmeDemoConfig.targetSupportingTypes,
         metadataResourcePath = AcmeDemoConfig.METADATA_RESOURCE_PATH
